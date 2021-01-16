d78b9c2ffc2b6d636fca3bc573d032482703112e
FISdiz <Saber9659>
2021-01-16 15:51:20 -0300

    test instrumental funcionando

--- app/src/androidTest/java/cl/salazarfelipe/felipesalazartd/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cl/salazarfelipe/felipesalazartd/ExampleInstrumentedTest.kt
@@ -1,20 +1,57 @@
 package cl.salazarfelipe.felipesalazartd
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import cl.salazarfelipe.felipesalazartd.model.database.BookDao
+import cl.salazarfelipe.felipesalazartd.model.database.BookDatabase
+import cl.salazarfelipe.felipesalazartd.model.database.BookEntity
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TestInstrumental {
 
-    @Test
-    fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("cl.salazarfelipe.felipesalazartd", appContext.packageName)
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    private lateinit var bookDao: BookDao
+    private lateinit var bookDB: BookDatabase
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        bookDB = Room.inMemoryDatabaseBuilder(context, BookDatabase::class.java).build()
+        bookDao = bookDB.getBookDao()
     }
 
+    @After
+    fun tearDown() {
+        bookDB.close()
+    }
+
+    @Test
+    fun insertBook() = runBlocking {
+        //Given
+        val bookList =
+            listOf(BookEntity(5, "Felipe", "Chile", "https://123Entel", "Español", "asinomas"))
+
+        //When
+        bookDao.insertBook(bookList)
+
+        //Then
+        bookDao.getAllBooks().observeForever {
+            assertThat(it).isNotNull()
+            assertThat(it).isNotEmpty()
+            assertThat(it).hasSize(1)
+        }
+    }
 }
